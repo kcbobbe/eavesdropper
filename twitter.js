1a8868ff7ac6581c4d76285ce63759c63dc89c8e
hsw919 <hunterwoody@gmail.com>
2018-05-26 13:57:39 -0400

    added styling to the twitter section

--- twitter.js
+++ twitter.js
@@ -15,16 +15,14 @@ $('#search').on('submit', (e) => {
       $('.content').html('')
       for (var i = 0; i < tweets.statuses.length; i++) {
         $('.content').append(`
-        <a href="https://twitter.com/${tweets.statuses[i].user.screen_name}/status/${tweets.statuses[i].id_str}" target="_blank">
+        <a class="twitter-link" href="https://twitter.com/${tweets.statuses[i].user.screen_name}/status/${tweets.statuses[i].id_str}" target="_blank">
           <div class="tweet">
             <img class="tweet__img" src="${tweets.statuses[i].user.profile_image_url}">
-            <h1 class="tweet__name">${tweets.statuses[i].user.name}</h1>
-            <h2 class="tweet__screen-name">@${tweets.statuses[i].user.screen_name}</h2>
+            <span class="tweet__name">${tweets.statuses[i].user.name}</span>
+            <span class="tweet__screen-name">@${tweets.statuses[i].user.screen_name}</span>
             <p class="tweet__content">${tweets.statuses[i].full_text}</p>
-            <div class="tweet__clout">
-              <span class="rt"><i class="fas fa-retweet"></i> ${tweets.statuses[i].retweet_count}</span>
-              <span class="fav"><i class="far fa-heart"></i> ${tweets.statuses[i].favorite_count}</span>
-            </div>
+              <span class="tweet__rt"><i class="fas fa-retweet"></i> ${tweets.statuses[i].retweet_count}</span>
+              <span class="tweet__fav"><i class="far fa-heart"></i> ${tweets.statuses[i].favorite_count}</span>
           </div>
         </a>
         `)
