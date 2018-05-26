fdc13bd09e5f5c34d4fbe1d4ae86395a4e3f9623
emachaffie <emachaffie@gmail.com>
2018-05-26 09:20:41 -0400

    Committing so I can merge new master, no major changes on flickr branch at this time.

--- flickr.js
+++ flickr.js
@@ -10,7 +10,7 @@ $('#search').on('submit', (e) => {
   // $('#flickr-photo-display-area').html('')
   const search = $('#input').val().split(' ').join('+')
   request
-    .get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=16e988fdeac45c2f91be93eb25384173&text=${search}&per_page=4&page=1&format=json&nojsoncallback=1&content_type=1&min_upload_date=1500000000`)
+    .get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=16e988fdeac45c2f91be93eb25384173&text=${search}&per_page=4&page=1&format=json&nojsoncallback=1&media=photos&min_upload_date=1500000000`)
     // .auth('16e988fdeac45c2f91be93eb25384173', '46661d21626543cd')
     .then(response => {
       console.log(response)
