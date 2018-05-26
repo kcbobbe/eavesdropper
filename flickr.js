ffdc04da0107017b55988c33d328b1a5087e0aba
emachaffie <emachaffie@gmail.com>
2018-05-26 10:50:37 -0400

    Further changes for still non-functioning slideshow

--- flickr.js
+++ flickr.js
@@ -54,17 +54,18 @@ function plusSlides (n) {
   showSlides(slideIndex += n)
 }
 
-// Thumbnail image controls
-function currentSlide (n) {
-  showSlides(slideIndex = n)
-}
+// // Thumbnail image controls
+// function currentSlide (n) {
+//   showSlides(slideIndex = n)
+// }
 
 function showSlides (n) {
   var i
-  var slides = document.getElementsByClassName('mySlides')
+  var slides = $('.mySlides')
+  console.log(slides)
   if (n > slides.length) { slideIndex = 1 }
   if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none'
+    slides[i].display = 'none'
   } slides[ slideIndex - 1 ].style.display = 'block'
 }
